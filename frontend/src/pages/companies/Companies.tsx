1b5bc33c58f1fb4d838801370317f8bf7c00dece
VG_Vengatesh <vengateshvg93@gmail.com>
2026-07-10 14:37:53 +0530

    feat: added Headers and Footers

--- frontend/src/pages/companies/Companies.tsx
+++ frontend/src/pages/companies/Companies.tsx
@@ -1,7 +1,20 @@
-
+import CompaniesFooter from "./components/CompaniesFooter";
+import CompaniesHeader from "./components/CompaniesHeader";
+import CompaniesTable from "./components/CompaniesTable";
+import CompaniesTableRow from "./components/CompaniesTableRow";
 function Companies() {
   return (
-    <div>Companies</div>
+ <div className="flex w-full  ms-auto h-full flex-col py-1  bg-white">
+      <CompaniesHeader />
+      <CompaniesTable />
+      <CompaniesTableRow />
+
+      <div className="flex-1 overflow-hidden">
+      </div>
+      <div className="mb-2">
+      <CompaniesFooter/>
+      </div>
+    </div>
   )
 }
 
